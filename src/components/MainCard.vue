d254e61dc5367ed760d64dc6f5b6107fb5fbacda
JackEdwardLyons <jack.edward.lyons@gmail.com>
2018-07-21 14:29:02 +1000

    style: minor card css updates

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -3,16 +3,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-10 col-sm-12 offset-md-1">
-                    <div class="pricing-item text-center">
+                    <div class="project-item text-center">
                         <div class="custom-card card card-outline-primary h-100">
                             <div class="card-body bg-primary">
                                 <h6 class="text-white">Filter Results</h6>
-                                <form class="justify-content-center d-flex">
-                                    <div class="form-row align-items-center">
-                                        <div class="col-auto my-1">
+                                <form class="container d-flex">
+                                    <div class="form-row row justify-content-center">
+                                        <div class="col-sm-2 col-md-2 my-1">
                                             <input type="text" class="form-control" id="title" placeholder="Title">
                                         </div>
-                                        <div class="col-auto my-1">
+                                        <div class="col-sm-3 col-md-2 my-1">
                                             <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
                                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
                                                 <option selected>
@@ -29,7 +29,7 @@
                                                 </option>
                                             </select>
                                         </div>
-                                        <div class="col-auto my-1">
+                                        <div class="col-sm-2 col-md-2 my-1">
                                             <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
                                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
                                                 <option selected>
@@ -70,7 +70,7 @@
                                                 </option>
                                             </select>
                                         </div>
-                                        <div class="col-auto my-1">
+                                        <div class="col-sm-2 col-md-2 my-1 mb-4">
                                             <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
                                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
                                                 <option selected>
@@ -90,7 +90,7 @@
                                                 </option>
                                             </select>
                                         </div>
-                                        <div class="col-auto my-1">
+                                        <div class="col-sm-2 col-md-2">
                                             <button class="btn btn-success btn-sm" type="submit">Filter</button>
                                         </div>
                                     </div>
@@ -101,41 +101,8 @@
                                     <div class="card-body">
                                         <a href="https://codepen.io/JackEdwardLyons/pen/bEpPqB" target="_blank">
                                             <h4 class="card-title">
-                                                Random Quote Machine
-                                                <small class="author sm-50">
-                                                    posted by Jack Lyons
-                                                </small>
-                                            </h4>
-                                        </a>
-                                        <h6 class="card-subtitle mb-2 text-muted">
-                                            This is a meta description of max 80 characters. This is as long as it can get.
-                                        </h6>
-                                        <p class="card-text">
-                                            Some quick example text to build upon the title and give readers a better description.
-                                            It can only be 120 characters. After that there will be a read-more button. <b class="c-pointer">Show more</b>
-                                        </p>
-                                        <div class="item-meta d-flex justify-content-between">
-                                            <span class="item-meta__likes text-center">
-                                                <span class="badge badge-info vote-count mb-1-xs">410 Votes</span>
-                                                <span class="vote-add">
-                                                    <i class="fa fa-thumbs-up"></i>
-                                                </span>
-                                            </span>
-                                            <span class="item-meta__tags-list ml-1-xs">
-                                                <b>Tags:</b>
-                                                <a href="#" class="card-link">jQuery, Bootstrap </a>
-                                                <span class="ml-2 badge badge-warning mt-1-xs pull-right">Front End</span>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </li>
-
-                                <li class="list-group-item">
-                                    <div class="card-body">
-                                        <a href="https://codepen.io/JackEdwardLyons/pen/bEpPqB" target="_blank">
-                                            <h4 class="card-title">
                                                 Full Stack Twitter Clone
-                                                <small class="author sm-50">
+                                                <small class="author sm-60">
                                                     posted by Jack Lyons
                                                 </small>
                                             </h4>
@@ -149,16 +116,18 @@
                                         </p>
                                         <div class="item-meta d-flex justify-content-between">
                                             <span class="item-meta__likes text-center">
-                                                <span class="badge badge-info vote-count mb-1-xs">43 Votes</span>
+                                                <span class="badge badge-info vote-count mr-1 mb-1-xs">43 Votes</span>
                                                 <span class="vote-add">
                                                     <i class="fa fa-thumbs-up fa-thumbs-up-xs"></i>
                                                 </span>
                                             </span>
-                                            <span class="item-meta__tags-list ml-1-xs">
-                                                <b>Tags:</b>
-                                                <a href="#" class="card-link">JavaScript, React, Node, Bootstrap</a>
-                                                <span class="ml-2 badge badge-danger mt-1-xs pull-right">Full Stack</span>
-                                            </span>
+                                            <div class="item-meta__tags-list ml-1-xs">
+                                                <p>
+                                                    <b>Tags: &nbsp;</b>
+                                                    <a href="#" class="card-link">JavaScript, React, Node, Bootstrap</a>
+                                                </p>
+                                                <span class="font-xs-60 ml-2 badge badge-danger mt-1-xs pull-right">Full Stack</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </li>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history', // get rid of url hash, which leverages history.pushState
   routes: [
     {
       path: '/',
