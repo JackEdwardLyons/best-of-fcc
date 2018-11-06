77bdd4bff17e91e370b5e3683344301fdf1b0daf
JackEdwardLyons <jack.edward.lyons@gmail.com>
2018-11-06 20:07:03 +1000

    Style: Minor modal font size increase and add extra notes to 'about' modal.

--- src/components/Modals/AboutModal.vue
+++ src/components/Modals/AboutModal.vue
@@ -3,7 +3,10 @@
       <main class="about-modal-main">
         <h2>About this App</h2>
         <p>
-          I built this app as a way to learn both Vue.js and Firebase Firestore.
+          I built this app as a way to learn both <a href="http://vuejs.org/" target="_blank">Vue.js</a> and 
+          <a href="https://www.google.com/search?q=firestore&ie=utf-8&oe=utf-8&client=firefox-b-ab" target="_blank">Firebase Cloud Firestore.</a>
+        </p>
+         <p>
           As a front end developer I wanted an easy way to explore some of the basic features
           found in all full-stack web applications. That's why I chose Firebase. It's simple to
           setup and provides a ton of powerful features without the need to write any server-side code.
@@ -14,10 +17,15 @@
           Those users who are not authentiacated can view the list of projects but cannot vote on a project.
         </p>
         <p>
-          - associating logged in users votes with the id's of the projects in the list in order to build a tally of votes for that project.
+          I also learned about fetching and filtering data from Firestore. While the filters on this site aren't overly complex, I still did 
+          gain a lot of insights into how database queries work. One thing I think Firestore can improve on is to increase
+          the variety of queries that are possible. For example, a simple search query is not possible without a third party
+          library. 
         </p>
         <p>
-          I also learned about fetching and filtering data from Firestore.
+          Overall, this was a really fun project to build. Please feel free to share this app with your friends. If you have any
+          feedback please <a href="https://www.freecodecamp.org/forum/u/JackEdwardLyons" target="_blank">
+          click here to send me a message on the Free Code Camp Forum.</a>
         </p>
       </main>
     </modal>
@@ -34,5 +42,9 @@ export default {
   padding: 1rem 2rem;
   color: #000;
   text-align: center;
+}
+
+p {
+  font-size: 125%;
 }
 </style>
